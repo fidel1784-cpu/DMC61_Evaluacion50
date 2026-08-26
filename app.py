20f221985219260edef8514e3a236a5e969ca126
fidel1784-cpu <fidel1784@gmail.com>
2026-08-25 23:43:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 st.title("Especializacion Python for Analitics, evaluacion 1")
 st.sidebar.title("Parámetros")
+st.sidebar.selectbox("Parámetros_")
 st.write("Elaborado por Fidel Bringas")
